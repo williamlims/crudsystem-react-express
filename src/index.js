5cde6861c54591aa9945e42c5c308495c78f437f
William Lima <williamlims@hotmail.com>
2021-09-14 00:29:27 -0300

    testing layout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,26 +14,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ContainedButtons() {
+export default function SimpleContainer() {
   const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <Button variant="contained">Default</Button>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        Secondary
-      </Button>
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button variant="contained" color="primary" href="#contained-buttons">
-        Link
-      </Button>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        <div>
+          <Button variant="contained">Default</Button>
+          <Button variant="contained" color="primary">
+            Primary
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary
+          </Button>
+          <Button variant="contained" disabled>
+            Disabled
+          </Button>
+          <Button variant="contained" color="primary">
+            Link
+          </Button>
+        </div>
+      </Container>
+    </React.Fragment>
   );
 }
 
-ReactDOM.render(<ContainedButtons />, document.getElementById('root'));
+ReactDOM.render(<SimpleContainer />, document.getElementById('root'));
